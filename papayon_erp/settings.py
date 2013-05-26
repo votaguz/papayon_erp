a35de2bec5f23d7480b711e5470ede9d7a86cd28
edgar287 <edgarblancopautt@gmail.com>
2013-05-26 00:51:25 -0500

    actualizando la configuracion de papayon

--- papayon_erp/settings.py
+++ papayon_erp/settings.py
@@ -17,7 +17,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'papayon_admin',
         'PASSWORD': 'papayonerp',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
