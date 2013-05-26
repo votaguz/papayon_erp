9b3134eaeb65fa8b7be03b3867343557ab41f5b5
Votaguz <votaguz@gmail.com>
2013-05-26 01:55:50 -0400

    Models Base

--- erp/models.py
+++ erp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class ProviderEntity(models.Model):
+    pass
+
+class CityEntity(models.Model):
+    pass
+
+class DepartmentEntity(models.Model):
+    pass
+
+class CountryEntity(models.Model):
+    pass
+    
+class ContactEntity(models.Model):
+    pass
+    
+class DocumentEntity(models.Model):
+    pass
+    
+class PurchaseOrderEntity(models.Model):
+    pass
+    
+class ItemEntity(models.Model):
+    pass
+    
+class ItemInPurchaseOrderEntity(models.Model):
+    pass
+
